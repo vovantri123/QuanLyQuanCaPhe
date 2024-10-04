7e94f05f8573d80e2969b8b9d0605b5f0765c369
vovantri123 <vovantri204@gmail.com>
2024-10-04 15:41:45 +0700

    ...

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyQuanCaPhe.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace QuanLyQuanCaPhe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fTrangChu());
+            Application.Run(new fDangNhap());
         }
     }
 }
